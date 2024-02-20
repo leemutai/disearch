5119537aa8a8f3d931ac9da6f0ef3ac3b3f3a261
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-02-20 05:03:33 +0300

    first comit

---
+++ app/src/test/java/com/leemutaisapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.leemutaisapplication.app
+
+import com.leemutaisapplication.app.appcomponents.base.BaseActivity
+import com.leemutaisapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
